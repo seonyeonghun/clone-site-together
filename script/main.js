492f53a43c5fec0b636faf2890943b0a86439059
seonyeonghun <sprax@naver.com>
2023-01-10 23:55:34 +0900

    메인|공통헤더 내비게이션작업

--- script/main.js
+++ script/main.js
@@ -1,6 +1,8 @@
+let isHit = false;
 window.addEventListener("DOMContentLoaded", function () {
   // HTML을 완전히 불러온 뒤 아래 메소드를 호출하는 이벤트
   modal();
+  gnb();
   // mainNav();
   // carousel();
 });
@@ -15,5 +17,22 @@ function modal() {
   });
   CLOSER.addEventListener("click", function () {
     MODAL.classList.add("d-none");
+  });
+}
+
+function sub_bg() {
+  const header = document.querySelector("header");
+  const div = document.createElement("div");
+  div.className = "gnb_bg";
+  header.appendChild(div);
+}
+
+function gnb(hit, gnb_sub) {
+  const GNB = document.querySelector("#gnb");
+}
+function gnbShow() {
+  const lnbAll = document.querySelectorAll(".lnb");
+  lnbAll.forEach(function (i) {
+    lnbAll[i].classList.add = "d-block";
   });
 }
