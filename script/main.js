8d05b0e66030422ff67841a9b1d21612790a7ac6
seonyeonghun2 <kjca1117@naver.com>
2023-01-10 18:33:52 +0900

    메인 | 공통헤더 추가작업 진행중

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", function () {
   // HTML을 완전히 불러온 뒤 아래 메소드를 호출하는 이벤트
   modal();
-  // mainNav();
+  mainNav();
   // carousel();
 });
 
@@ -16,4 +16,27 @@ function modal() {
   CLOSER.addEventListener("click", function () {
     MODAL.classList.add("d-none");
   });
+}
+function mainNav() {
+  const headerNAV = document.querySelector(".header-nav");
+  headerNAV.addEventListener("mouseenter", function(){
+    addNavBg();
+  });
+  // headerNAV.addEventListener("mouseleave", function(){
+  //   removeNavBg();
+  // });
+}
+/**
+ * 내비게이션 배경을 동적으로 추가하는 함수
+ */
+function addNavBg() {
+  const HEADER = document.querySelector("header");
+  const UL = document.querySelectorAll(".lnb");
+  const div = document.createElement("div");
+  div.className = "header_bg";
+  HEADER.appendChild(div);
+  UL.forEach(function(item) {
+    // item.classList.add = "d-block";
+    console.log(item);
+  })
 }
