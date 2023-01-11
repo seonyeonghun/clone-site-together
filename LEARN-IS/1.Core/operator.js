b9a5c01d4f452bc777aab59d090ac4b813da0ccf
kimej2159 <kimej2159@naver.com>
2023-01-11 17:40:31 +0900

    notice 뉴스룸 김은지 2023-01-11

---
+++ LEARN-IS/1.Core/operator.js
@@ -0,0 +1,21 @@
+//연산자 : 사칙연산~ java의 그것과 (거의) 같음
+// +,-,%,*, /,**
+//console.log(Math.pow(5,2));
+console.log(5**2);
+
+//문자열 연결 연산자
+console.log("가나다"+123);
+
+let num1 = 10;
+num1++;
+console.log("num1 :" +num1);
+num1=++num2;
+console.log("num2 :" +num2);
+console.log("num1 :" +num1);
+
+//대입, 복합 대입연산자 
+// =, +=,-=,*=,/* ...
+// a += 1; <--> a = a+1과 같음
+
+// 비교연산자 : ture, false
+// >, :
